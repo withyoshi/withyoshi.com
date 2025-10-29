9f54e6957c01897b85b39e2ab373718bcbf7fc50
Yoshi Nakamoto <yoshi@nkmt.to>
2025-10-29 20:44:28 +0800

    Upgrade to Next.js 16.

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { type NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
   const siteDomain = process.env.NEXT_PUBLIC_SITE_DOMAIN;
 
