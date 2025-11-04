fddf4b1d1bf9336e16467e681f2280cd861850ea
Yoshi Nakamoto <yoshi@nkmt.to>
2025-11-04 13:13:37 +0800

    Making sure copying messages doesn't include img alt messages.

--- app/cv/chatbot/chat-toggle.tsx
+++ app/cv/chatbot/chat-toggle.tsx
@@ -41,7 +41,7 @@ export function ChatToggle() {
         <div className="absolute top-0 right-0 h-full w-[150%] overflow-hidden rounded-r-full">
           <div className="absolute right-0 aspect-square h-full min-h-full">
             <Image
-              alt="Chat with Yoshi"
+              alt=""
               className={`${isOpen ? "translate-x-[100%] scale-0" : "-translate-x-[10px] scale-96"} absolute top-0.5 left-0 h-full w-full transform transition-transform duration-500 ease-out`}
               height={24}
               src="/images/cv-yoyo.svg"
